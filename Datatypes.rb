30dc5fc0c3a72ae02888c3ed09d97de3418bc4a3
Sameena <thabu.sami@gmail.com>
2018-01-16 14:30:56 +0530

    data types program in ruby

---
+++ Datatypes.rb
@@ -0,0 +1,16 @@
+
+# Declare second integer, double, and String variables.
+a = 1
+b = 1.0
+c = 'Hello'
+# Read and save an integer, double, and String to your variables.
+a = gets.to_i
+b = gets.to_f
+c = gets
+# Print the sum of both integer variables on a new line.
+puts (a + i)
+# Print the sum of the double variables on a new line.
+puts (d + b)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+puts (s + c)
